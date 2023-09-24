7200a8c436411b70561fec64515adc83f98f47f9
rohitpeter <rohitpeter2580@gmail.com>
2023-09-24 16:06:56 +0530

    initial commit

---
+++ src/Assignment1/DeletingFile.java
@@ -0,0 +1,26 @@
+package Assignment1;
+import java.io.File;
+import java.io.IOException;
+
+public class DeletingFile {
+    public static void main(String[] args) throws IOException{
+        // TODO Auto-generated method stub
+        try
+        {
+            File myFile = new File("file1.txt");
+            if(myFile.exists())
+            {
+                myFile.delete();
+                System.out.println("File deleted Successfully...");
+                ;			}
+            else
+            {
+                System.out.println("File DO NOT Exist");
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
